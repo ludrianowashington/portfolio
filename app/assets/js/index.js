a34d14d4b32eaab53e8e34a1b509f5a8cbb3a8bb
ludrianowashington <ludriano.washi@gmail.com>
2020-06-14 14:51:32 -0300

    Adição de imagens e ajuste dos dados

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -4,3 +4,13 @@ window.onload = () => {
       "\n Amanhã terei algo mais para te mostrar!! UM ABRAÇÃO!!"
   );
 };
+
+window.onscroll = () => {
+  let navbar = document.getElementById("ld-header");
+
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    navbar.style.backgroundColor = "#1E1E25";
+  } else {
+    navbar.style.backgroundColor = "#00000000";
+  }
+};
