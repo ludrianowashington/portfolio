99110a6b7733d9c83a4552ee9a9a5aeb1c1acf00
ludrianowashington <ludriano.washi@gmail.com>
2020-07-14 22:31:25 -0300

    Inserção da seção SOBRE

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,10 +1,3 @@
-window.onload = () => {
-  alert(
-    "Desculpe-me, ainda não está pronto!!\n" +
-      "\n Amanhã terei algo mais para te mostrar!! UM ABRAÇÃO!!"
-  );
-};
-
 window.onscroll = () => {
   let navbar = document.getElementById("ld-header");
 
@@ -14,11 +7,3 @@ window.onscroll = () => {
     navbar.style.backgroundColor = "#00000000";
   }
 };
-
-function clicked() {
-  let navItem = document.getElementsByClassName("nav-item");
-  let section = [0];
-
-  console.log(navItem);
-}
-document.body;
